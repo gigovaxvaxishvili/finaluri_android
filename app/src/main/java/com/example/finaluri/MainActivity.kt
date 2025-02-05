f1711d86c4cd82d5a9808a747ccabce49f810262
gigov <giorgi.vakhvakhishvili.1@btu.edu.ge>
2025-02-05 23:07:56 +0400

    finaluri

---
+++ app/src/main/java/com/example/finaluri/MainActivity.kt
@@ -0,0 +1,49 @@
+package com.example.finaluri
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.finaluri.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        replaceFragment(Home())
+
+        binding.bottomNavigationView.setOnItemSelectedListener {
+
+            when(it.itemId){
+
+                R.id.home -> replaceFragment(Home())
+                R.id.Favorite -> replaceFragment(Favorite())
+                R.id.Shoppingcart -> replaceFragment(ShoppingCart())
+
+                else ->{
+
+
+
+                }
+
+            }
+
+            true
+
+        }
+
+    }
+
+    private fun replaceFragment(fragment : Fragment){
+
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_layout, fragment)
+        fragmentTransaction.commit()
+
+    }
+
+}
